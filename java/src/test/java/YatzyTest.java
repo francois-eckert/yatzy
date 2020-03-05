d8a664485f1a4c9288d9a490823f20321503132b
francois <francois.eckert@gmail.com>
2020-03-05 18:21:22 +0100

    Init structure du projet Maven et Junit 5

--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -1,5 +1,7 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class YatzyTest {
 
