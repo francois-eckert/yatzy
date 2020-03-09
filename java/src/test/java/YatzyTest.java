308d20f4aa213347236abacdb99e5b52e857ab1d
francois <francois.eckert@gmail.com>
2020-03-09 09:27:13 +0100

    refacto chance

--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -24,11 +24,10 @@ public class YatzyTest {
     }
 
     @Test
+    @DisplayName("chance scores sum of all dice")
     public void chance_scores_sum_of_all_dice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
-        assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3,3,4,5,1));
+        Yatzy yatzy = new Yatzy(2,3,4,5,1);
+        assertEquals(15, yatzy.chance());
     }
 
     @Test public void yatzy_scores_50() {
